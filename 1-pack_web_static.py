51a8ed2089d48ff78c70e8ab267ca26d6bc3649c
xica369 <761@holbertonschool.com>
2019-08-20 04:48:33 +0000

    T1: file

---
+++ 1-pack_web_static.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Fabric script that generates a .tgz archive from the contents of the web_static """
+from fabric.api import local
+
+
+def do_pack():
+	""" function do pack"""
+	return None
