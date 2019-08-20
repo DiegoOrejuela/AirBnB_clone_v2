82923612f47d7de4f503ef39b7e293cc89766cf7
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-08-20 04:30:24 +0000

    T1: Fabric script that generates a .tgz archive from the contents of the web_static

---
+++ 1-pack_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module 1-pack_web_static.py"""
+from fabric.api import local, settings
+import os
+
+
+def do_pack():
+    """Fabric script that generates a .tgz archive from
+    the contents of the web_static folder"""
+
+    if not os.path.isdir("versions"):
+        local("mkdir versions")
+
+    with settings(warn_only=True):
+        cm = "tar -czvf versions/web_static_$(date +'%Y%m%d%H%M%S').tgz"
+        cm = cm + " /data/web_static"
+        result = local(cm, capture=True)
+        if result.return_code == 0:
+            list_files = os.listdir("versions")
+            return list_files[0]
+        else:
+            return None
