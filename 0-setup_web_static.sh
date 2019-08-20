ff129d1a1a91ed5e3f306789fb0de9b696e047b2
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-08-20 00:56:32 +0000

    T0: delete = location

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -19,5 +19,5 @@ ln -sf /data/web_static/releases/test /data/web_static/current
 chown -R ubuntu /data
 chgrp -R ubuntu /data
 sudo sed -i "38i\\
-\tlocation = /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-enabled/default
+\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-enabled/default
 sudo service nginx restart
