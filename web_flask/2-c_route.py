c2c1b7cdfca11f78459ac739a918948624da5fee
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-09-03 20:38:18 +0000

    T2: display “C ” followed by the value of the text variable

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Module 2-c_route"""
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/c/<string:text>')
+def c(text):
+    """c - display “C ” followed by the value of the text variable
+    Return: string with format"""
+
+    if "_" in text:
+        text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route('/hbnb')
+def HBNB():
+    """HBNB - show acronym Holberton School
+    Return: string 'HBNB'"""
+    return 'HBNB'
+
+
+@app.route('/')
+def hello_HBNB():
+    """hello_HBNB - say hello since Holberton School
+    Return: string with greeting"""
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
