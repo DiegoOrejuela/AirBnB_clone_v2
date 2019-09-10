34f83db7ffa0b7fc0dcf6825ebd465d4b8f3016a
xica369 <761@holbertonschool.com>
2019-09-10 03:42:54 +0000

    T10: update

--- models/place.py
+++ models/place.py
@@ -5,6 +5,11 @@ from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from sqlalchemy.orm import relationship
 import os
 
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60), ForeignKey('places.id'), nullable=False),
+                      Column('amenity_id', String(60), ForeignKey('amenities.id'), nullable=False))
+)
+
 
 class Place(BaseModel, Base):
     """This is the class for Place
